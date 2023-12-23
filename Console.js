7a0de74a2ff85d0fcd377712e15b8466dde46ee6
Raviteja <rtg74163@gmail.com>
2023-12-24 00:23:32 +0530

    day 1 JS coding

---
+++ Console.js
@@ -0,0 +1,12 @@
+//single line comments
+/* multi-line
+comments*/
+//to show outoput on console window 
+console.log("ravi");
+console.log('teja');
+//save file with .js extension for javaScript file
+//HTML is already conneted to browser 
+//JS need to connect to browser via HTML by script tage
+let a = 'Ravi';
+document.write('Raviteja');//to print on browser window
+console.log('teja');//to print on console of browser
