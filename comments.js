e2ae8263bd042baff35e39103fc77e2e65c3b011
Raviteja <rtg74163@gmail.com>
2023-12-24 14:27:06 +0530

    comments file added

---
+++ comments.js
@@ -0,0 +1,10 @@
+//to know other dev what is this code we will include comments
+//comments will not be processed by compiler
+//comments are two types single line comments and multi line comments
+/* this is 
+multi line 
+comment*/
+//this is single comment
+//comments makes readability of the code
+//comments should not explain the code line by line 
+//comments should tell the logic of the code
