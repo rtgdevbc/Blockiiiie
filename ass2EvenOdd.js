5eae38af8c9850c752d3cd7a2d31c5c866525dfc
Raviteja <rtg74163@gmail.com>
2024-01-01 00:23:19 +0530

    assignments in JavaScript

---
+++ ass2EvenOdd.js
@@ -0,0 +1,7 @@
+let num = 1;
+if (num % 2 === 0){
+    console.log(`${num} is a Even`);
+}
+else {
+    console.log(`${num} is an Odd number`);
+}
---
+++ ass3SI.js
@@ -0,0 +1,5 @@
+let principle = 100000;
+let rateOfInt = 12;
+let timePeriod = 4;
+console.log(`Simple Intereset is ${(principle*(rateOfInt/100)*3)} for ${timePeriod} years with ${rateOfInt}%`);
+console.log(`Total Amount is ${(principle + (principle*(rateOfInt/100)*3))} for ${timePeriod} years with ${rateOfInt}%`);
