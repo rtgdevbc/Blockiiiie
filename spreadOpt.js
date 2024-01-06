bcedf069b4eb980118ed8f4332c6ff7ff6c399a0
Raviteja <rtg74163@gmail.com>
2024-01-06 17:26:34 +0530

    spread modified

--- spreadOpt.js
+++ spreadOpt.js
@@ -7,39 +7,4 @@ function restSpreadOpt (...num){ //rest op
 }
 console.log(restSpreadOpt(1,2,3,4,5));
 
-// call()
 
-const obj1 = {
-    x : "you are so sweet ",
-    y : "you are so bad",
-    z : function(){
-        return this.x + this.y;
-    }
-}
-console.log(obj1.z());
-
-const obj2 = {
-    x : "you are actually mental ",
-    y : "Need to take care",
-}
-
-console.log(obj1.z.call(obj2));
-
-// apply()
-
-const obj3 = {
-    x : "Ravi ",
-    y : "Teja",
-    z : function(a, b){
-        return a + b;
-    }
-}
-
-console.log(obj3.z());
-
-const obj4 = {
-    x : 1,
-    y : 2
-}
-
-console.log(obj3.z.apply([obj4,1,2]));
