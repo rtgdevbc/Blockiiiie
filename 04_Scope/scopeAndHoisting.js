4820525f10e2c7d1d819b705f1246e519d4d3ef6
Raviteja <rtg74163@gmail.com>
2024-01-15 20:28:56 +0530

    Temporal Dead Zone added

--- 04_Scope/scopeAndHoisting.js
+++ 04_Scope/scopeAndHoisting.js
@@ -56,18 +56,32 @@ if(true){
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //++++++++++++++++++++++++++++++++++++++++++++HOISTING+++++++++++++++++++++++++++++++++++++++++++
 
-console.log(addOne(6)); //7 hoisting of fuction takes place and fucntion declaration will allow this
+// console.log(addOne(6)); //7 hoisting of fuction takes place and fucntion declaration will allow this
 //we can call the fuction before declaring it is possible in JS because of Hoisting
 function addOne (num){
     return num+1;
 }
 //---------------------------------------------------------------------------------------------------
-console.log(addTwo(6)); //error (ReferenceError: Cannot access 'addTwo' before initialization)
+// console.log(addTwo(6)); //error (ReferenceError: Cannot access 'addTwo' before initialization)
 const addTwo = function (num){
     return num + 1;
 }
-console.log(addTwo(6)); //7
+// console.log(addTwo(6)); //7
 //above type is function expression
+
+//--------------------------- LET & CONST will Hoist ----------------------------
+console.log(a); //ReferenceError: Cannot access 'a' before initialization
+let a = 10;
+/**
+ * here a got hoisted but put in a diff memory called Temporal Dead Zone we
+ * cannot access them without initilazation 
+ * TEMPORAL DEAD ZONE: is time from which hoisting of let and const to initilazation 
+ * of variables till then we can't access varaibles 
+ * in the above example we cannot access 'a' till line 74 execution.
+ * once line 74 executed we can access the variable a 
+ */
+
+
 
 
 
